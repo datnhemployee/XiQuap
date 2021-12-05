685302507b78e60ff5ccb3ad49e89965f5862042
datnh <datnh@dgroup.co>
2021-12-05 09:04:54 +0700

    Added ts, socketio

--- App.tsx
+++ App.tsx
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,7 +17,11 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+type SectionProps = {
+  title: string;
+};
+
+const Section: React.FunctionComponent<SectionProps> = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -52,7 +47,9 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
+type AppProps = {};
+
+const App: React.FunctionComponent<AppProps> = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
